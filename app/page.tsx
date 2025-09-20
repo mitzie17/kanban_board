55093edd763686b52ca99977bb0147f3798e9a4f
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-09-20 16:20:01 -0700

    import Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "./Components/navbar/navbar";
 
 export default function Home() {
   return (
     <>
-      <h1 className="michroma">My Kanban Board</h1>
+      <Navbar />
     </>
   );
 }
