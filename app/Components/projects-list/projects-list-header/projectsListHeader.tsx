ad763c20446cfb012c798e78d5a8a69aeb8aed0e
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-09-20 23:08:10 -0700

    import SortingDropdown and Button components

--- app/Components/projects-list/projects-list-header/projectsListHeader.tsx
+++ app/Components/projects-list/projects-list-header/projectsListHeader.tsx
@@ -1,4 +1,6 @@
 import { MdOutlineSortByAlpha } from "react-icons/md";
+import SortingDropdown from "../../drop-downs/sortingDropdown";
+import { Button } from "@/components/ui/button";
 
 export default function ProjectsListHeader() {
   return (
@@ -7,12 +9,18 @@ export default function ProjectsListHeader() {
       <div className="flex gap-3 items-center">
         <span className="text-2xl font-bold">Projects</span>
       </div>
+
       {/* Div contains label, icon, dropdown */}
       <div className="flex items-center gap-2">
+        {/* Inner div contains label and icon */}
         <div className="flex items-center gap-1">
           <MdOutlineSortByAlpha className="text-xl text-gray-500" />
           <span className="text-gray-500 text-sm">Sort</span>
         </div>
+
+        {/* Sorting and new task button */}
+        <SortingDropdown />
+        <Button className="rounded-3xl px-4">Add Task</Button>
       </div>
     </div>
   );
