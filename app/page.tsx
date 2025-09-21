f8d142e8b56000119be23d0d465dfc3c515810d5
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-09-20 17:10:29 -0700

    import useTheme, and import ProjectsList component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
+"use client";
+
+import { useTheme } from "next-themes";
 import Navbar from "./Components/navbar/navbar";
+import ProjectList from "./Components/projects-list/projectList";
 
 export default function Home() {
+  const { theme } = useTheme();
+  const backgroundColor = theme === "dark" ? "bg-black" : "bg-gray-200";
   return (
-    <>
+    <div className={`${backgroundColor} border min-h-screen w-full`}>
       <Navbar />
-    </>
+      <div className="grid grid-cols-[3fr_1fr] px-6 mt-8 gap-4 michroma">
+        <ProjectList />
+      </div>
+    </div>
   );
 }
