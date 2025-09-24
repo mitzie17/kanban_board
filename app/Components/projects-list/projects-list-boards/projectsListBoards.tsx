587d47fca81eac097368f867394019012e55eb4e
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-09-23 22:58:26 -0700

    create the SingleBoard component that renders each of the three boards

--- app/Components/projects-list/projects-list-boards/projectsListBoards.tsx
+++ app/Components/projects-list/projects-list-boards/projectsListBoards.tsx
@@ -1,3 +1,16 @@
+import SingleBoard from "./singleBoard";
+
 export default function ProjectsListBoards() {
-  return <div></div>;
+  const boards: Board[] = [
+    { name: "Yet to start", createdAt: new Date(), tasks: [] },
+    { name: "In progress", createdAt: new Date(), tasks: [] },
+    { name: "Completed", createdAt: new Date(), tasks: [] },
+  ];
+  return (
+    <div className="h-full rouded-2xl flex items-center mt-4 gap-3">
+      {boards.map((board, index) => (
+        <SingleBoard key={index} board={board} />
+      ))}
+    </div>
+  );
 }
