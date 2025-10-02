d458d6653e4068e5b25fd599363f775cf2a73275
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-10-01 23:37:48 -0700

    uncommented ProjectSelection component

--- app/Components/right-side-bar/rightSidebar.tsx
+++ app/Components/right-side-bar/rightSidebar.tsx
@@ -1,12 +1,13 @@
 import { Card } from "@/components/ui/card";
 import CircularProgress from "./circularProgress";
 import TasksStats from "./tasksStats";
+import ProjectSelection from "../drop-downs/project-selection/projectSelection";
 
 export default function RightSidebar() {
   return (
     <Card className="shadow-none p-6 rounded-3xl max-h-[640px]">
       <div className="flex flex-col gap-0">
-        {/* <ProjectSelectionDropdown /> */}
+        <ProjectSelection />
         <CircularProgress />
         <TasksStats />
       </div>
