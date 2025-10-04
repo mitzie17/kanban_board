32f78985f63d7edd5ba2ed995f4edcb089d6987c
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-10-03 22:58:18 -0700

    define props pass through SingleProjectItem component

--- app/Components/drop-downs/project-selection/singleProjectItem.tsx
+++ app/Components/drop-downs/project-selection/singleProjectItem.tsx
@@ -1,7 +1,16 @@
-export default function SingleProjectItem() {
-  return (
-    <div>
-      <div> Single Project Item</div>
-    </div>
-  );
+import { CommandItem } from "@/components/ui/command";
+import { Project, projects } from "./projectSelection";
+
+export default function SingleProjectItem({
+  project,
+  isSelected,
+  onSelectedItem,
+}: {
+  project: Project;
+  isSelected: boolean;
+  onSelectedItem: (project: Project) => void;
+}) {
+  // Destructure the project object
+  const { name: projectName, tasks, icon: ProjectIcon } = project;
+  return <CommandItem>hello</CommandItem>;
 }
