7cdbfd0cfeb15f96323d53d5bb409614e6f0eb4d
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-10-03 22:57:19 -0700

    import Command component from shadcn

--- app/Components/drop-downs/project-selection/projectCommandItems.tsx
+++ app/Components/drop-downs/project-selection/projectCommandItems.tsx
@@ -1,7 +1,43 @@
-export default function ProjectCommandItems() {
+import { Dispatch, SetStateAction } from "react";
+import { Project, projects } from "./projectSelection";
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandList,
+} from "@/components/ui/command";
+import SingleProjectItem from "./singleProjectItem";
+
+export default function ProjectCommandItems({
+  selectedProject,
+  setSelectedProject,
+}: {
+  selectedProject: Project;
+  setSelectedProject: Dispatch<SetStateAction<Project>>;
+}) {
+  function handleProjectSelect(project: Project) {
+    setSelectedProject(project);
+  }
   return (
-    <div>
-      <div> ProjectCommandItems</div>
-    </div>
+    <Command>
+      <CommandInput placeholder="Search a project..." />
+      <CommandList className="my-3">
+        <CommandEmpty>No results found.</CommandEmpty>
+      </CommandList>
+      <div>
+        {/* map through each projects array and call it the singleProject command item and pass the props */}
+        {projects.map((project, index) => (
+          <SingleProjectItem
+            // project item
+            project={project}
+            key={index}
+            // callback function
+            onSelectedItem={handleProjectSelect}
+            // the isSelected based on the condition of selected project name is equal of the project name in the array
+            isSelected={selectedProject.name === project.name}
+          />
+        ))}
+      </div>
+    </Command>
   );
 }
