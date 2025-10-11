87bfdfbe7b960479671cf338d471435149f5d59a
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-10-10 22:10:23 -0700

    add cicular progress bar from docs

--- app/Components/right-side-bar/circularProgress.tsx
+++ app/Components/right-side-bar/circularProgress.tsx
@@ -1,7 +1,31 @@
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
 export default function CircularProgress() {
+  const percentage = 77;
+  const primaryColor = "#15A25E";
   return (
-    <div>
-      <div>Circular Progress</div>
-    </div>
+    <CircularProgressbar
+      className="text-primary size-52 my-10"
+      value={percentage}
+      text={`${percentage}%`}
+      styles={{
+        // customize the path (progress bar)
+        path: {
+          stroke: primaryColor, // change the progressbar color
+          strokeLinecap: "round", // rounded edges
+          transition: "stroke-dashoffset 0.5s ease 0s", // smooth transition
+        },
+        // customize the trail (background)
+        trail: {
+          stroke: "#e2e8f0", // change the background color
+        },
+        // customize the text
+        text: {
+          fill: primaryColor, // change the text color
+          fontSize: "16px", // change the font size
+        },
+      }}
+    />
   );
 }
